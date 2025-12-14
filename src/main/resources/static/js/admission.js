5c96e9d7b01fc984d99381af0bc8669423ee680d
shahebaz <shahebazkhan014@gmail.com>
2025-12-14 19:55:45 +0530

    admission cancellation functionality added

--- src/main/resources/static/js/admission.js
+++ src/main/resources/static/js/admission.js
@@ -547,6 +547,7 @@ function saveCancellationDetails() {
           "error",
           "Error saving cancellation details" ,
           "/admissions?id=" + $("#admissionId").val()
+
       );
       console.error(xhr);
     }
